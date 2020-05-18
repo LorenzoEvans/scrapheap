a25e3278b42f4d23da3a399e2d451029052c7eaa
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-05-17 21:58:45 -0700

    init commit

---
+++ fmt_act.rs
@@ -0,0 +1,14 @@
+use std::fmt;
+
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut dyn fmt::Formatter) -> fmt::Result {
+        write!(f,)
+    }
+}
+
+
+fn main() {
+
+};
