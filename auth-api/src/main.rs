76aa4646dd3a16e0b03fc8e21ae8897cbdf09cf9
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-05-21 14:25:28 -0700

    Dear god that was from 5 years ago.

--- auth-api/src/main.rs
+++ auth-api/src/main.rs
@@ -1,3 +1,24 @@
+#[macro_use] // Brings macros from Nickel into scope
+extern crate nickel;
+use nickel::{Nickel, JsonBody, HttpRouter, Request, Response, MiddlewareResult, MediaType}
 fn main() {
-    println!("Hello, world!");
+    
+    let mut server = Nickel::new();
+    let mut router = Nickel::router();
+    
+    router.get("/users", middleware! {|request, response|
+        format!("Hello from GET /users")
+    });
+
+    router.post("/users/new", middleware! {|request, response| // These pipes represent a Rust closure.
+        format!("Hello from POST /users")
+    });
+
+    router.delete("/users/:id", middleware! {|request, response|
+        format!("Hello from DELETE /users/:id")
+    });
+
+    server.utilize(router);
+
+    server.listen("127.0.0.1:9000");
 }
